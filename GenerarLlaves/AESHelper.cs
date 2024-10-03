4b03b2e8525603f0b4c292ce4a104d408e9b0b6f
Kevin Barrera <kevin.barre92@yahoo.com>
2024-10-03 17:26:11 -0600

    Initial Commit

---
+++ GenerarLlaves/AESHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace GenerarLlaves
+{
+    public class AESHelper
+    {
+        public static void GenerarClaveYIV(out string key, out string iv)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = 256; // AES-256
+                aes.GenerateKey();
+                aes.GenerateIV();
+
+                key = Convert.ToBase64String(aes.Key);
+                iv = Convert.ToBase64String(aes.IV);
+            }
+        }
+
+        public static string Encriptar(string plainText, string key, string iv)
+        {
+            byte[] keyBytes = Convert.FromBase64String(key);
+            byte[] ivBytes = Convert.FromBase64String(iv);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = keyBytes;
+                aes.IV = ivBytes;
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+                        cs.Write(plainBytes, 0, plainBytes.Length);
+                        cs.FlushFinalBlock();
+                        return Convert.ToBase64String(ms.ToArray());
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ GenerarLlaves/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerarLlaves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = "XSFu0BuSCe54F4ViMppMDPVmVfZIhchM2ElsX/eSlSI=";
+            string iv = "/pRr62jWZ4blEjlNshBr3w==";
+
+            //AESHelper.GenerarClaveYIV(out key, out iv);
+
+            Console.WriteLine(key);
+            Console.WriteLine(iv);
+
+            Console.WriteLine();
+            var cs = Console.ReadLine();
+
+            Console.WriteLine(AESHelper.Encriptar(cs, key, iv));
+            Console.ReadKey();
+        }
+    }
+}
